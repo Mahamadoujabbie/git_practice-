4593a3710816af6acad241eb046887c767d1d533
mahamadou jabbie <jabbiemahamadou40@gmail.com>
2025-01-08 04:43:27 +0000

    the second box is added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const mama = document.getElementById("mama");
+const mom = document.getElementById("mom");
 
 mama.addEventListener('click', lerting);
+mom.addEventListener('click', lerting)
 
 function lerting(){
-    alert("hello there");
+alert("hello")
 }
