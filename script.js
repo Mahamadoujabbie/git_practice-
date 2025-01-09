e41cd6a34e3022881ca05f4c01e24420ffc0db65
mahamadou jabbie <jabbiemahamadou40@gmail.com>
2025-01-09 04:05:32 +0000

    i give them border radius

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const mama = document.getElementById("mama");
-const mom = document.getElementById("mom");
-
-mama.addEventListener('click', lerting);
-mom.addEventListener('click', lerting)
-
-function lerting(){
-alert("hello")
-}
+$(function(){
+    $(".id").click(function(){
+        alert(this.textContent);
+    })
+})
